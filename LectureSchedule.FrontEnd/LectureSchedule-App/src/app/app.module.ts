427b0d48c16e72828bab4aa74a0492632250b421
ving-developer <henrique.sb0709@gmail.com>
2022-05-22 23:14:54 -0300

    feat(frontend): adding fontawesome and ngx-bootstrap
    -Run commands:
      >ng add @fortawesome/angular-fontawesome@latest
      >ng add ngx-bootstrap
    -Using bootstrap class in app.component.html

--- LectureSchedule.FrontEnd/LectureSchedule-App/src/app/app.module.ts
+++ LectureSchedule.FrontEnd/LectureSchedule-App/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LecturesComponent } from './lectures/lectures.component';
 import { SpeakerComponent } from './speaker/speaker.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { SpeakerComponent } from './speaker/speaker.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
