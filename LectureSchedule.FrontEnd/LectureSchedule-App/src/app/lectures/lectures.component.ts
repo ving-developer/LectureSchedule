6eefbbc9948246411f68471fda2343dc10f2c694
ving-developer <henrique.sb0709@gmail.com>
2022-05-24 19:54:11 -0300

    feat(frontend): adding Lecture module in app.component.html
    -Adding content of Lecture module
    -Including Lecture module in app.component.html

--- LectureSchedule.FrontEnd/LectureSchedule-App/src/app/lectures/lectures.component.ts
+++ LectureSchedule.FrontEnd/LectureSchedule-App/src/app/lectures/lectures.component.ts
@@ -7,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lectures.component.scss']
 })
 export class LecturesComponent implements OnInit {
-  public lectures: any;
+  public lectures: any = [];
+  public imageWidth = 100;
+  public imageMargin = 2;
+  public hideImage = true;
+  private _listFilter = '';
+  public filteredLectures: any = [];
+
+  public get listFilter(): string{
+    return this._listFilter;
+  }
+
+  public set listFilter(value: string){
+    this._listFilter = value;
+    this.filteredLectures = this.listFilter ? this.filterLectures(this._listFilter) : this.lectures;
+  }
+
+  filterLectures(filter: string): any{
+    filter = filter.toLowerCase();
+    return this.lectures.filter(
+      (lecture: any) => lecture.theme.toLowerCase().indexOf(filter) !== -1 ||
+            lecture.local.toLowerCase().indexOf(filter) !== -1
+    );
+  }
 
   constructor(private _http: HttpClient) { }
 
@@ -15,9 +37,16 @@ export class LecturesComponent implements OnInit {
     this.loadLectures()
   }
 
+  controlImageVisibility(){
+    this.hideImage = !this.hideImage;
+  }
+
   public loadLectures() : void {
     this._http.get('https://localhost:5001/Lectures').subscribe(
-      res => this.lectures = res,
+      res => {
+        this.lectures = res;
+        this.filteredLectures = res;
+      },
       error => console.log(error)
     );
   }
