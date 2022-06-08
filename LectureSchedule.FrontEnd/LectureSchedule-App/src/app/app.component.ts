bc673caed3263b38b9307f17f2c57692b102504e
ving-developer <henrique.sb0709@gmail.com>
2022-06-08 17:52:38 -0300

    feat(frontend): adding form in login.component

--- LectureSchedule.FrontEnd/LectureSchedule-App/src/app/app.component.ts
+++ LectureSchedule.FrontEnd/LectureSchedule-App/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LectureSchedule-App';
+  constructor(private router: Router){}
+
+  public showNav(): boolean{
+    return !["/user/login", '/user/registration'].includes(this.router.url);
+  }
 }
