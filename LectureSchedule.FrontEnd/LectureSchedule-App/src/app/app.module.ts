af59b11180bb93db2644edca2ffc4a909cc23069
ving-developer <henrique.sb0709@gmail.com>
2022-06-10 21:56:37 -0300

    feat(frontend): Implementing Reacitve-Form in lecture-details.component
    -Including form beholder of lecture-details.component with validators

--- LectureSchedule.FrontEnd/LectureSchedule-App/src/app/app.module.ts
+++ LectureSchedule.FrontEnd/LectureSchedule-App/src/app/app.module.ts
@@ -2,7 +2,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 //angular bootstrap
 import { CollapseModule } from 'ngx-bootstrap/collapse';
@@ -69,7 +69,8 @@ import { RegistrationComponent } from './components/user/registration/registrati
       preventDuplicates: true,
       progressBar: true
     }),
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    ReactiveFormsModule
   ],
   providers: [LectureService],
   bootstrap: [AppComponent],
--- LectureSchedule.FrontEnd/LectureSchedule-App/src/app/components/lectures/lecture-details/lecture-details.component.ts
+++ LectureSchedule.FrontEnd/LectureSchedule-App/src/app/components/lectures/lecture-details/lecture-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-lecture-details',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lecture-details.component.scss']
 })
 export class LectureDetailsComponent implements OnInit {
+  form?: FormGroup
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get f(): any{
+    return this.form?.controls
   }
 
+  constructor(private builder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  public validation(): void {
+    this.form = this.builder.group({
+      theme : ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+      local : ['', [Validators.required, Validators.minLength(4), Validators.maxLength(100)]],
+      date : ['', Validators.required],
+      maxPeopleSupported : ['', [Validators.required, Validators.max(120000)]],
+      adress : ['', Validators.required],
+      imageUrl : ['', Validators.required],
+      email : ['', [Validators.required, Validators.email]],
+      phone : ['', Validators.required],
+    });
+  }
+
+  public resetForm(){
+    this.form?.reset();
+  }
 }
