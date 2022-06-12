8b6d4fc1f74a0cac015e4cf983de679de8acc702
ving-developer <henrique.sb0709@gmail.com>
2022-06-12 13:29:25 -0300

    refactor(frontend): including limit calls in Observables
    -Including take() function to add call limit for lecture.service consumers

--- LectureSchedule.FrontEnd/LectureSchedule-App/src/app/services/lecture.service.ts
+++ LectureSchedule.FrontEnd/LectureSchedule-App/src/app/services/lecture.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, pipe, take } from 'rxjs';
 import { Lecture } from '@app/models/Lecture';
 
 @Injectable(
@@ -13,26 +13,38 @@ export class LectureService {
   constructor(private _http: HttpClient) { }
 
   getLectures(): Observable<Lecture[]>{
-    return this._http.get<Lecture[]>(this.baseUrl);
+    return this._http
+    .get<Lecture[]>(this.baseUrl)
+    .pipe(take(1));
   }
 
   getLecturesByTheme(theme: string): Observable<Lecture[]>{
-    return this._http.get<Lecture[]>(`${this.baseUrl}/search-theme?theme=${theme}`);
+    return this._http
+    .get<Lecture[]>(`${this.baseUrl}/search-theme?theme=${theme}`)
+    .pipe(take(1));;
   }
 
   getLectureById(id: number): Observable<Lecture>{
-    return this._http.get<Lecture>(`${this.baseUrl}/${id}`);
+    return this._http
+    .get<Lecture>(`${this.baseUrl}/${id}`)
+    .pipe(take(1));
   }
 
   post(lecture: Lecture): Observable<Lecture>{
-    return this._http.post<Lecture>(this.baseUrl, lecture);
+    return this._http
+    .post<Lecture>(this.baseUrl, lecture)
+    .pipe(take(1));
   }
 
   put(lecture: Lecture): Observable<any>{
-    return this._http.put<any>(`${this.baseUrl}/${lecture.id}`,lecture);
+    return this._http
+    .put<any>(`${this.baseUrl}/${lecture.id}`,lecture)
+    .pipe(take(1));
   }
 
   delete(id: number): Observable<any>{
-    return this._http.delete<any>(`${this.baseUrl}/${id}`);
+    return this._http
+    .delete<any>(`${this.baseUrl}/${id}`)
+    .pipe(take(1));
   }
 }
