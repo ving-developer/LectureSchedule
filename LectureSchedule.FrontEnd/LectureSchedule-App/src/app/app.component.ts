a89c6540a29ff5eda033062db8ad85294e7677ca
ving-developer <henrique.sb0709@gmail.com>
2022-08-14 17:45:15 -0300

    fix(frontend): preserve user state while page reloaded

--- LectureSchedule.FrontEnd/LectureSchedule-App/src/app/app.component.ts
+++ LectureSchedule.FrontEnd/LectureSchedule-App/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from './models/Identity/User';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +10,20 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'LectureSchedule-App';
-  constructor(private router: Router){}
+  constructor(
+    private router: Router,
+    public userService: UserService
+  ){}
+
+  public ngOnInit(): void {
+    this.setCurrentUser();
+  }
+
+  setCurrentUser(): void {
+    let user = (localStorage.getItem('user') ?
+      JSON.parse(localStorage.getItem('user')) : null) as User;
+    this.userService.setCurrentUser(user);
+  }
 
   public showNav(): boolean{
     return !["/user/login"].includes(this.router.url);
