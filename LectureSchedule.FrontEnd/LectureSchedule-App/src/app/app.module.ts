34fe2b3896b9aadc958a58d6d6b31cd988a4dc48
ving-developer <henrique.sb0709@gmail.com>
2022-08-14 21:55:24 -0300

    chore(frontend): updating icons and minors bugs

--- LectureSchedule.FrontEnd/LectureSchedule-App/src/app/app.module.ts
+++ LectureSchedule.FrontEnd/LectureSchedule-App/src/app/app.module.ts
@@ -1,5 +1,5 @@
 //angular
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
